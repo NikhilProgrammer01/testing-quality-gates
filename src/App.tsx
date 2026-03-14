a3f795d58e2950f851045265fe4fd63cf686a641
NikhilProgrammer01 <nikhilgohil316@gmail.com>
2026-03-14 18:50:00 +0530

    test changes

--- src/App.tsx
+++ src/App.tsx
@@ -165,7 +165,7 @@ function App() {
   return (
     <>
       <div className="greeting-bar">
-        <span data-testid="greeting">{getGreeting(now.getHours())}, have a great day!</span>
+        <span data-testid="greetingg">{getGreeting(now.getHours())}, have a great day!</span>
         <span data-testid="clock">{formatTime(now)}</span>
       </div>
 
